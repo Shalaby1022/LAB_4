e6ce7bddf72498f8502993b79d894c397779e1e9
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-07-12 15:06:01 +0300

    Add project files.

---
+++ LAB_4/Swapping.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace LAB_4
+{
+    internal class Swapping
+    {
+
+
+        static void Swap(int[] array, int index1, int index2)
+        {
+            if (index1 >= 0 && index1 < array.Length && index2 >= 0 && index2 < array.Length)
+            {
+                int temp = array[index1];
+                array[index1] = array[index2];
+                array[index2] = temp;
+                Console.WriteLine("Swapping successful!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid indices. Swapping failed!");
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of your array");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] students = new int[size];
+
+            for (int i=0; i<size; i++)
+            {
+                Console.WriteLine($"Enter the value for index {i+1}");
+                students[i] = int.Parse(Console.ReadLine());
+            }
+           Console.WriteLine("Array before swapping ");
+           PrintArray(students);
+
+            Console.WriteLine("Enter first index ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second index ");
+            int y = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("After Swapping ");
+            Swap( students, x, y);
+            PrintArray(students);
+
+           
+
+            
+
+
+
+
+        }
+    }
+}
---
+++ StudentClass/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace StudentClass
+{
+    internal class Program
+    {
+        class Student
+        {
+            private string ssn;
+            private string name;
+            private int age;
+            private string address;
+
+            public void SetSSn(string _ssn)
+            {
+                ssn = _ssn;
+
+            }
+           public void SetName(string _name)
+            {
+                name = _name;
+            }
+            public void SetAge(int _age)
+            {
+                age = _age;
+            }
+            public void SetAdress(string _address)
+            {
+                address = _address;
+            }
+            public string GetSsn()
+            {
+                return ssn;
+            }
+            public string GetName()
+            {
+                return name;
+            }
+            public int GetAge()
+            {
+                return age;
+            }
+            public string GetAdress()
+            {
+                return address;
+
+            }
+
+            public string PrintData()
+            {
+                return $"SSN is {ssn} , Name is {name} , Age is {age} , Address is {address}";
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            // One User 
+            Student student = new Student();
+
+            Console.WriteLine("Enter student details:");
+            Console.Write("SSN: ");
+            student.SetSSn(Console.ReadLine());
+
+            Console.Write("Name: ");
+            student.SetName(Console.ReadLine());
+
+            Console.Write("Age: ");
+            student.SetAge(int.Parse(Console.ReadLine()));
+
+            Console.Write("Address: ");
+            student.SetAdress(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Student Information:");
+            Console.WriteLine(student.PrintData());
+
+
+
+            // group of users 
+
+            Console.Write("Enter the number of students: ");
+            int numStudents = int.Parse(Console.ReadLine());
+
+            Student[] students = new Student[numStudents];
+
+            for (int i = 0; i < numStudents; i++)
+            {
+                Console.WriteLine($"Enter details for student {i + 1}:");
+                Student stu = new Student();
+
+                Console.Write("SSN: ");
+                student.SetSSn(Console.ReadLine());
+
+                Console.Write("Name: ");
+                student.SetName(Console.ReadLine());
+
+                Console.Write("Age: ");
+                student.SetAge(int.Parse(Console.ReadLine()));
+
+                Console.Write("Address: ");
+                student.SetAdress(Console.ReadLine());
+
+                students[i] = stu;
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Student Information:");
+            for (int i = 0; i < numStudents; i++)
+            {
+                Console.WriteLine($"Student {i + 1}:");
+                students[i].PrintData();
+            }
+
+
+        }
+
+
+
+
+    }
+}
